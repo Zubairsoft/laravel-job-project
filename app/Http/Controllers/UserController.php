5871de6191f360aadf07447f008ee71201aba68b
mohammed zubair <moh.775123554@gmail.com>
2022-04-09 01:23:35 +0300

    migration table

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    // function used for display show table
     function show(){
         return view('dashboard.add.users');
     }
